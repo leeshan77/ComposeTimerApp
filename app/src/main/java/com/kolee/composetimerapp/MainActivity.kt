bf89c3a926006a6191760557d25a548fa77a7614
KOLEE <leeshan77@naver.com>
2024-06-08 19:11:50 +0900

    template

---
+++ app/src/main/java/com/kolee/composetimerapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.kolee.composetimerapp
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import com.kolee.composetimerapp.ui.theme.ComposeTimerAppTheme
+
+
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            ComposeTimerAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Text(text = "Compose Timer App")
+                }
+            }
+        }
+    }
+}
